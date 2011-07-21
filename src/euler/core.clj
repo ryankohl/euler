f0f4dca8fba3df72c77aaeda0abd8486f267518c
Ryan <ryan@ubuntu.ubuntu-domain>
2011-07-20 20:19:23 -0400

    euler 5 soln

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,66 +1,81 @@
 (ns euler.core)
 (use 'clojure.contrib.math)
 
-(defn euler1a [num]
+(defn div35? [num]
+  "divisible by 3 or 5"
   (or (= (mod num 3) 0) 
       (= (mod num 5) 0)))
 (defn euler1 [num]
-  (reduce + (filter euler1a (range num))))
+  "the sum of numbers less than 100 divisible by 3 or 5"
+  (reduce + (filter div35? (range num))))
 
-(defn euler2a [ [oldsum x y]]
+(defn fib [ [oldsum x y]]
+  "fibonacci"
   (let [z (+ x y)
         newsum (cond (even? y) (+ oldsum y)
                      (odd? y) oldsum)]
     [newsum y z]))
 (defn euler2 [max]
+  "sum of even-valued fibonacci terms less than 4x10^6"
   (loop [ans [0 1 1]]
     (if (> (last ans) max)
       (first ans)
-      (recur (euler2a ans)))))
+      (recur (fib ans)))))
   
-(defn euler3a [factor num]
+(defn fac-out [factor num]
   "factor out the factor"
   (loop [ans num]
     (if (< 0 (mod ans factor))
       ans
       (recur (/ ans factor)))))
 (defn euler3 [num]
+  "find the largest prime factor of a composite number (600851475143)"
   (loop [factor 1
          ans num]
     (let [f (+ 1 factor)
-          x (euler3b f ans)]
+          x (fac-out f ans)]
       (if (= x 1) f (recur f x)))))
 
-(defn euler4a [num]
-  "how big is the number?"
-  (loop [s 1]
-    (if (= num (mod num (expt 10 s)))
-      (- s 1)
-      (recur (+ s 1)))))
-(defn euler4b [num]
-  (loop [e (euler4a num)
-         n num
-         a []]
-    (if (< e 0)
-      a
-      (recur (- e 1) (rem n (expt 10 e)) (conj a (quot n (expt 10 e)))))))
-(defn euler4c [num]
-  "for i in 0..size/2 (rounded down), see if num[i] == num[size-i]"
-  (let [n (euler4b num)
-        s (euler4a num)]
-    (every? true? (map #(= (nth n %) (nth n (- s %))) (range 0 (Math/ceil (/ s 2)))))))
-(defn euler4 [s]
-  "find the largest palindrome that's the product of two numbers with s-many digits"
-  (loop [x (expt 10 s)
-         a []]
-    (if (= x 0)
-      (reduce max a)
-      (let [ b  (reduce max
-                        (conj (filter euler4c
-                                (map (partial * (- x 1))
-                                     (range (expt 10 (- s 1))
-                                            (expt 10 s))))
-                              0))]
-           (recur (- x 1) (conj a b))))))
+(defn palin? [x]
+  "is this number a palindrome?"
+  (= (seq (str x)) (reverse (seq (str x)))))
+(defn euler4 [x]
+  "find the largest palindrome that's the product of 2 x-digit numbers"
+  (let [M (- (expt 10 x) 1)
+        m (- (expt 10 (- x 1)))]
+  (->> (for [x (range M m -1)
+             y (range M (dec x) -1)]
+         (* x y))
+       (filter #(palin? %))
+       (take 20)
+       (apply max))))
 
-
+(defn lcd [n t]
+  (loop [a t]
+    (cond (> (* a a) n) n
+          (= (rem n a) 0) a
+          (= 2 a) (recur 3)
+          true (recur (+ a 2)))))
+(defn prime? [x] (= x (lcd x 2)))
+(defn next-prime-factor [x]
+  (loop [a 2]
+    (cond (and (prime? a) (= (rem x a) 0)) a
+          true (recur (inc a)))))
+(defn get-subvec [s S]
+  "ensure that s is a sub-vector of S (adding elements of s when needed)"
+  (loop [a s
+         b S]
+    (cond (= 0 (rem (reduce * b) (reduce * s))) b
+          true (recur (pop a) (conj b (last a))))))
+(defn get-factors [x]
+  "get a list of factors of x"
+  (loop [a []
+         b x]
+    (cond (prime? b) (conj a b)
+          true (recur (conj a (next-prime-factor b)) (/ b (next-prime-factor b))))))
+(defn euler5 [x]
+  "what is the smallest PosInt that is evenly divisible by all the ints from 1 to 20?"
+  (loop [a []
+         b (vec (map get-factors (range 1 (inc x))))]
+    (cond (empty? b) (reduce * a)
+          true (recur (get-subvec (last b) a) (pop b)))))
